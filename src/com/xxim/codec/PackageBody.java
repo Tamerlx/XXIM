b335cfb483b33a8d118456e20466cd6a8dff39fc
--help <liuxu@APPLEdeMacBook-Pro.local>
2017-11-20 23:39:12 +0800

    progress pbmessage

--- src/com/xxim/codec/PackageBody.java
+++ src/com/xxim/codec/PackageBody.java
@@ -1,5 +1,16 @@
 package com.xxim.codec;
 
-public class PackageBody {
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.xxim.codec.protobuf.PBMessage;
 
+public class PackageBody {
+	
+	public PBMessage.pbmessage decode(byte[] input) throws InvalidProtocolBufferException {
+		return PBMessage.pbmessage.parseFrom(input);
+		
+	}
+	
+	public void encode() {
+		
+	}
 }
