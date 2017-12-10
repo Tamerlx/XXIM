8cc25cffce990bda8e912fa8c005a27dc35e5b86
--help <liuxu@APPLEdeMacBook-Pro.local>
2017-12-10 17:38:59 +0800

    completed

--- src/com/xxim/codec/PackageConst.java
+++ src/com/xxim/codec/PackageConst.java
@@ -1,7 +1,7 @@
 package com.xxim.codec;
 
 public class PackageConst {
-	public static final byte MAGIC_SOH = (byte) 0x01;
+	public static final byte MAGIC_SOH = (byte)0x01;
 	public static final byte MAGIC_STX = (byte)0x02;
 	public static final char PACKAGE_HEADER_VERSION = '1';
 	public static final char PACKAGE_HEADER_SECTYPE = '1';
--- src/com/xxim/transport/handler/ClientHandler.java
+++ src/com/xxim/transport/handler/ClientHandler.java
@@ -2,9 +2,11 @@ package com.xxim.transport.handler;
 
 import com.xxim.client.AbstractClient;
 import com.xxim.codec.Package;
+import com.xxim.codec.PackageBody;
 import com.xxim.codec.PackageDecoder;
 import com.xxim.codec.PackageHeader;
 import com.xxim.codec.PackageType;
+import com.xxim.codec.protobuf.PBMessage;
 
 public class ClientHandler implements IProtocol {
 	
@@ -32,9 +34,17 @@ public class ClientHandler implements IProtocol {
 					// 1. bytes正好只含有body所有数据 return IForwardMessageStatus.COMPLETED;
 					// 2. bytes未含有body所有数据 return IForwardMessageStatus.UNCOMPLETED;
 					// 3. bytes含有body所有数据且有剩余，接续解析;
+					PackageHeader header = currentPackage.getHeader();
+					System.out.println((char)header.magicHeader_0);
+					System.out.println((char)header.magicHeader_1);
+					int bodylength = header.bodyLength;
+					PackageBody body = new PackageBody();
+					byte[] bytes_body = coder.unpackBytes(bodylength);
+					PBMessage.pbmessage  pmsg= body.decode(bytes_body);
 					
+					System.out.println(pmsg);
 				}
-				System.out.println(ret);
+//				System.out.println(ret);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
